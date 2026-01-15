ca91f3668d32a6fe7ddc46e8c39eda18cee57377
Tushar Babar <babartushar560@gmail.com>
2026-01-15 14:20:43 +0530

    Update resume link in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ function Hero() {
         </p>
 
         <div className="hero-buttons slide-up">
-          <a href="/Tushar-Babar.pdf" className="btn primary">Download Resume</a>
+          <a href="/Tushar-B-Resume.pdf" className="btn primary">Download Resume</a>
           <a href="#projects" className="btn secondary">View Projects</a>
         </div>
 
